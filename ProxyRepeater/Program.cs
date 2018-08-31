97b195a2decfbab6f7c7af937550357dbb3a69ed
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-08-31 02:08:32 -0500

    Finishing scaffolding

--- ProxyRepeater/Program.cs
+++ ProxyRepeater/Program.cs
@@ -4,21 +4,39 @@ namespace ProxyRepeater
 {
     internal class Program
     {
-        private static void Main(string[] args)
-        {
-            Parser.Default.ParseArguments<CommandLineArguments>(args)
-                .WithParsed(o =>
-                {
-                });
-        }
-
         private class CommandLineArguments
         {
-            [Option(HelpText = "Set port to listen from services")]
+            [Option(HelpText = "Set port to listen from services" , Default = 8888)]
             public int Port { get; set; }
 
-            [Option(HelpText = "Set port to receive commands from clients")]
+            [Option(HelpText = "Set port to receive commands from clients" , Default = 8080)]
             public int WebApiPort { get; set; }
+        }
+
+        private static void Main(string[] args) =>
+            Parser.Default.ParseArguments<CommandLineArguments>(args)
+               .WithParsed(o =>
+               {
+                   SetupDependencies();
+                   TurnOnProxy(o.Port);
+                   TurnOnWebApi(o.WebApiPort);
+
+                   while (true) ;
+               });
+
+        private static void TurnOnWebApi(int webApiPort)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void TurnOnProxy(int proxyPort)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void SetupDependencies()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
