3574e61b7f0ec5d45a949c3657be0d39c1057e18
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-09-12 02:12:50 -0500

    Scaffolding IExchanger

---
+++ ProxyRepeater.Server.Implementations/MsgClientDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using ProxyRepeater.Server.Core;
+using System;
+using System.Collections.Generic;
+
+namespace ProxyRepeater.Server.Implementations
+{
+    public class MsgClientDispatcher : IExchanger
+    {
+        public ErrorNumber AddClient(ExClient client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearClients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ErrorNumber DeleteClient(ExClient client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeliverMessage(IClientMsg msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ExClient> GetClients()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
