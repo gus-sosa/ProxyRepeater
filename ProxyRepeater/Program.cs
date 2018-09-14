4dc64ab4bd7f3c55726e0c0b6c94e5aa57c33ded
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-09-14 16:23:56 -0500

    First implementation of the server

--- ProxyRepeater/Program.cs
+++ ProxyRepeater/Program.cs
@@ -1,12 +1,23 @@
 ﻿using CommandLine;
+using LightInject;
 using Nancy.Hosting.Self;
+using ProxyRepeater.Server.Core;
+using ProxyRepeater.Server.Implementations;
 using System;
+using System.Net;
+using System.Threading.Tasks;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Models;
 
 namespace ProxyRepeater.Server
 {
     internal class Program
     {
         public static NancyHost WebApiHost { get; private set; }
+        public static ServiceContainer Container { get; set; } = new ServiceContainer();
+
+        public static ProxyServer ProxyServer { get; set; }
 
         private class CommandLineArguments
         {
@@ -28,6 +39,8 @@ namespace ProxyRepeater.Server
                    Console.ReadLine();
 
                    WebApiHost.Stop();
+                   ProxyServer.Stop();
+                   Container.Dispose();
                });
 
         private static void TurnOnWebApi(int webApiPort)
@@ -40,12 +53,22 @@ namespace ProxyRepeater.Server
 
         private static void TurnOnProxy(int proxyPort)
         {
-            throw new System.NotImplementedException();
+            ProxyServer = new ProxyServer();
+            ProxyServer.CertificateManager.TrustRootCertificate();
+            IMsgDeliverer deliverer = Container.GetInstance<IMsgDeliverer>();
+            ProxyServer.AfterResponse += (object sender , SessionEventArgs e) => Task.Run(() => deliverer.DeliverMessage(new SessionAdapter(e)));
+            var explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any , proxyPort , true);
+            ProxyServer.AddEndPoint(explicitEndPoint);
+            Console.WriteLine($"Starting proxy at: http://localhost:{proxyPort}/");
+            ProxyServer.Start();
         }
 
-        private static void SetupDependencies()
-        {
-            throw new System.NotImplementedException();
-        }
+        private static void SetupDependencies() =>
+            Container
+                .Register(_ => new MsgClientDispatcher() , new PerContainerLifetime())
+                .Register<IClientMsg , ClientMessage>()
+                .Register<IExchanger>(factory => factory.GetInstance<MsgClientDispatcher>())
+                .Register<IMsgDeliverer>(factory => factory.GetInstance<MsgClientDispatcher>())
+                .Register<IProxy , TitaniumProxyServer>();
     }
 }
